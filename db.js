765309807e9c7debb0f018e406c4e9ebd888e6b7
MinhNguyet-Love <minhnguyet22072004@gmail.com>
2025-11-01 00:34:04 +0700

    update db.js to use DATABASE_URL

--- db.js
+++ db.js
@@ -1,14 +1,9 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
 
-dotenv.config(); // 🔹 giúp đọc biến từ file .env
+dotenv.config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect((err) => {
   if (err) {
