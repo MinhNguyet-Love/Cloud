0e7756e1205661735b0b854ada18f0cbb1a5ff20
MinhNguyet-Love <minhnguyet22072004@gmail.com>
2025-11-14 11:05:04 +0700

    test1

--- db.js
+++ db.js
@@ -15,7 +15,33 @@
 
 // export default pool.promise();
 // db.js
-import mysql from "mysql2";
+// import mysql from "mysql2";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || "mysql.railway.internal",
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT || 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// // Test kết nối tự động
+// pool.getConnection((err, conn) => {
+//   if (err) {
+//     console.error("❌ Lỗi kết nối MySQL Pool:", err);
+//   } else {
+//     console.log("✅ Đã kết nối MySQL Pool thành công!");
+//     conn.release();
+//   }
+// });
+
+// export default pool;
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -30,14 +56,13 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-// Test kết nối tự động
-pool.getConnection((err, conn) => {
-  if (err) {
-    console.error("❌ Lỗi kết nối MySQL Pool:", err);
-  } else {
-    console.log("✅ Đã kết nối MySQL Pool thành công!");
-    conn.release();
-  }
-});
+// Test kết nối
+try {
+  const conn = await pool.getConnection();
+  console.log("✅ MySQL Promise Pool Connected!");
+  conn.release();
+} catch (err) {
+  console.error("❌ Lỗi kết nối MySQL:", err);
+}
 
 export default pool;
